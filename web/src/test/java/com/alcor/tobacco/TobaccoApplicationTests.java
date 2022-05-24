18621ffd81d79cc1b1be40e0ce7de14d4cd50bb2
guchun <1084259073@qq.com>
2022-05-24 20:08:50 +0800

    first code submit

---
+++ web/src/test/java/com/alcor/tobacco/TobaccoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alcor.tobacco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobaccoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
