f01e47449a41ca9b99c3f61e389a3c283c6d8871
guchun <1084259073@qq.com>
2022-05-25 13:31:06 +0800

    edit dependency Demo Controller

---
+++ web/src/main/java/com/alcor/tobacco/controller/DemoController.java
@@ -0,0 +1,21 @@
+package com.alcor.tobacco.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author guchun
+ * @description TODO
+ * @date 2022/5/24 20:09
+ */
+@RestController
+@RequestMapping("/hello")
+public class DemoController {
+
+    @GetMapping("/pi")
+    public String hello() {
+        return "po";
+    }
+
+}
